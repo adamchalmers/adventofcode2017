fc05bda98946d0723f66e63719e97e3d37299d13
Adam Chalmers <achalmers@rmn.com>
2018-01-26 18:24:37 -0600

    Day 10 p2 WIP

--- day10.hs
+++ day10.hs
@@ -1,13 +1,48 @@
-import Data.List
+import Data.Char
+import Data.Bits
+import Prelude hiding (group)
+import Numeric (showHex, showIntAtBase)
 
-hash nums lens = p * q
+type NumRange = [Int]
+
+multiHash :: NumRange -> [Int] -> NumRange
+multiHash nums lens =
+    multiHash' 0 0 nums lens 16
+
+group :: Int -> [Int] -> [[Int]]
+group n = foldr f [] where
+    f x [] = [[x]]
+    f x output@(o:os) =
+        if (length o) == n
+        then ([x]):output
+        else (x:o):os
+
+showHex' i = twoLong (showHex i "")
+twoLong [x] = "0" ++ [x]
+twoLong xs = xs
+
+showDense :: [Int] -> String
+showDense xs = concat $ map showHex' xs
+
+sparseToDense :: NumRange -> [Int]
+sparseToDense sparse = map (foldr1 xor) (group 16 sparse)
+
+multiHash' :: Int -> Int -> NumRange -> [Int] -> Int -> NumRange
+multiHash' curr skip nums lens 0 = nums
+multiHash' curr skip nums lens n = 
+    multiHash' curr' skip' nums' lens (n-1)
     where
-        (p:q:_) = fst $ twist nums lens
+        (nums', curr', skip') = twist curr skip nums lens
+
+hash curr skip nums lens = p * q
+    where
+        (p:q:_) = fst $ twist curr skip nums lens
         fst (x,_,_) = x
 
-twist nums lens = foldl (twistAcc) (nums, 0, 0) lens
+twist :: Int -> Int -> NumRange -> [Int] -> (NumRange, Int, Int)
+twist curr skip nums lens = foldl (twistAcc) (nums, curr, skip) lens
 
-twistAcc :: ([Int], Int, Int) -> Int -> ([Int], Int, Int)
+twistAcc :: (NumRange, Int, Int) -> Int -> (NumRange, Int, Int)
 twistAcc (nums, curr, skip) len = (nums', curr', skip') where
     n     = length nums
     nums' = reverseFrom nums curr len
@@ -15,10 +50,7 @@ twistAcc (nums, curr, skip) len = (nums', curr', skip') where
     skip' = (skip + 1)          `rem` n
 
 -- Reverse the list from element #i to #(i + len), working circularly.
--- 1. Rotate the list so that #i -> #0
--- 2. Rotate from 0 to len
--- 3. Rotate the list #0 -> #i
-reverseFrom :: [Int] -> Int -> Int -> [Int]
+reverseFrom :: NumRange -> Int -> Int -> [Int]
 reverseFrom nums i len = 
     (rotR i) . (reverseFirst len) . (rotL i) $ nums
     where
@@ -29,7 +61,12 @@ reverseFrom nums i len =
 main = do
     unitTests
     let input = [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110]
-    print $ hash [0..255] input
+    let soln1 = hash 0 0 [0..255] input
+    putStrLn $ "Soln1: " ++ (show soln1)
+    --soln2
+    let inputAscii = (map ord "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110") ++ [17,31,83,47,23]
+    let soln2 = showDense $ sparseToDense $ multiHash [0..255] inputAscii
+    putStrLn $ "Soln2: " ++ (soln2)
 
 unitTests = do
     print $ (reverseFrom [0..4]      0 3) == [2,1,0,3,4]
@@ -42,5 +79,12 @@ unitTests = do
     print $ (twistAcc ([4,3,0,1,2], 3, 2) 1) == ([4,3,0,1,2], 1, 3)
     print $ (twistAcc ([4,3,0,1,2], 1, 3) 5) == ([3,4,2,1,0], 4, 4)
 
-    print $ (twist [0..4] [3,4,1,5]) == ([3,4,2,1,0], 4, 4)
-    print $ (hash [0..4] [3,4,1,5]) == 12
+    print $ (twist 0 0 [0..4] [3,4,1,5]) == ([3,4,2,1,0], 4, 4)
+    print $ (hash 0 0 [0..4] [3,4,1,5]) == 12
+
+    print $ group 16 [0..15] == [[0..15]]
+    print $ group 16 [0..31] == [[0..15], [16..31]]
+
+    let someInts = [65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22] :: [Int]
+    print $ (foldr1 xor someInts) == 64
+
