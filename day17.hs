2bf947939c6e676ee903ba6cdea04634f4110483
Adam Chalmers <adam.s.chalmers@gmail.com>
2018-02-14 01:08:48 -0600

    Refactored day 17 solution

--- day17.hs
+++ day17.hs
@@ -4,26 +4,32 @@ import Prelude hiding (take, drop, length, (++))
 
 offset = 344
 
-simulateSpinlock :: (a -> Int -> Int -> a) -> Int -> Int -> Int -> a -> a
-simulateSpinlock f last i val answer =
-    if last == val
-    then answer
-    else simulateSpinlock f last i' (val + 1) $ f answer i' val
-        where
-            i' = ((i + offset) `rem` val) + 1
+simulate :: (a -> Int -> Int -> a) -> Int -> a -> a
+simulate f numIters partialSoln = simulate' 0 1 partialSoln
+    where
+        simulate' i val partialSoln =
+            if val == numIters
+            then partialSoln
+            else simulate' i' (val + 1) partialSoln'
+                where
+                    i' = ((i + offset) `rem` val) + 1
+                    partialSoln' = f partialSoln i' val
 
-ans1 vec i' val = (take i' vec) ++ singleton val ++ (drop i' vec)
-ans2 afterZero i' val = if i' == 1 then val else afterZero
+ans1 vec i' val =
+    take i' vec ++ singleton val ++ drop i' vec
+
+ans2 afterZero i' val =
+    if i' == 1 then val else afterZero
+
+valAfter :: (Unbox a, Eq a) => a -> Vector a -> Maybe a
+valAfter val xs = fmap (\i -> xs ! (i + 1)) (elemIndex val xs)
 
 main = do
-    let iters = 2018
-    let spinlock = simulateSpinlock ans1 iters 0 1 (singleton 0)
-    let i = fmap (\i -> spinlock ! (i + 1)) (elemIndex (iters - 1) spinlock)
-
+    let soln1 = simulate ans1 2018 (singleton 0)
     putStrLn "Soln 1"
-    print i
+    print $ valAfter 2017 soln1
 
-    let soln2 = simulateSpinlock ans2 50000000 0 1 1
+    let soln2 = simulate ans2 5000 1
     putStrLn "Soln 2"
     print soln2
 
