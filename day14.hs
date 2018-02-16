9048ed552af479288c9e889b616a1fdb9d4acd37
Adam Chalmers <achalmers@rmn.com>
2018-02-15 18:05:04 -0600

    Finished day 14

--- day14.hs
+++ day14.hs
@@ -6,13 +6,10 @@ import Data.Vector as V
 import qualified Data.List as L
 import Prelude hiding (map, filter, length, (++), concatMap, sum, take)
 
-data Cell = Free | Used deriving (Eq)
-instance Show Cell where
-    show Free = "."
-    show Used = "#"
+-----------------------------------------
+-- Part 1
 
-type Grid a = Vector (Vector a)
-type Point = (Int, Int)
+data Cell = Free | Used deriving (Eq)
 
 makeGrid s = map (toRow . makeInput) (fromList [0..127]) where
     makeInput i =
@@ -40,39 +37,64 @@ makeGrid s = map (toRow . makeInput) (fromList [0..127]) where
         'e' -> "1110"
         'f' -> "1111"
 
-index grid (x,y) = (grid ! x) ! y
+
+-----------------------------------------
+-- Part 2
+
+
+type Grid a = Vector (Vector a)
+type Point = (Int, Int)
+
+get grid (x,y)   = (grid ! x) ! y
+set grid (x,y) v = grid // [(x, (grid ! x) // [(y,v)])]
 
 countCC :: Grid Cell -> Int
-countCC grid = labelGrid grid (0,0) 1
+countCC grid =
+    countCC' grid (0,0) 1
+        where
+            countCC' grid p@(x,y) counter
+                | x == (length grid)       = counter - 1
+                | get grid p == Free = countCC' grid (step grid p) counter
+                | otherwise = countCC' grid' (step grid p) (counter + 1)
+                    where
+                        grid' = dfs grid p
 
 step :: Grid a -> Point -> Point
 step grid (x,y)
     | y == (length grid - 1) = (x+1, 0)
     | otherwise              = (x, y+1)
 
-labelGrid :: Grid Cell -> Point -> Int -> Int
-labelGrid grid p@(x,y) counter
-    | x == (length grid)       = counter
-    | index grid p == Free = labelGrid grid (step grid p) counter
-    | otherwise = labelGrid grid' (step grid p) (counter + 1)
-        where
-            found = dfs grid [p] []
-            grid' = markFree found grid
-
-markFree :: [Point] -> Grid Cell -> Grid Cell
--- TODO: Mark all points in `found` as Free
-markFree found = id
-
-dfs :: Grid Cell -> [Point] -> [Point] -> [Point]
-dfs grid (q:qs) found =
-    -- TODO: Run a DFS from `p` that only visits neighbours if they're used.
-    found
+dfs :: Grid Cell -> Point -> Grid Cell
+-- DFS from the current cell in all 4 directions,
+-- only visiting Used nodes and marking them as Free once they've been visited.
+dfs grid p@(x,y)
+    | get grid p == Free = grid
+    | otherwise =
+        let
+            dirs =
+                [ (x+1,y)
+                , (x-1,y)
+                , (x,y+1)
+                , (x,y-1)
+                ]
+            pointFilter (x,y) =
+                   x >= 0
+                && y >= 0
+                && x < length grid
+                && y < length grid
+            validDirs = L.filter pointFilter dirs
+            grid' = set grid p Free
+        in
+        L.foldl dfs grid' validDirs
 
 main = do
     let example = "flqrgnkx"
     let real = "jxqlasbh"
-    let grid = makeGrid $ fromList example
+    let grid = makeGrid $ fromList real
     putStrLn "Soln1"
     print $ sum $ map (length . filter (== Used)) grid
+    putStrLn "Soln2"
+    print $ countCC grid
+
 
 
